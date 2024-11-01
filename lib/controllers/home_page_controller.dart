edefecc1329032b7378147666d34a99e2548c62d
shayanalvi <shayanalvi6@gmail.com>
2024-11-01 22:19:52 +0500

    first commit

---
+++ lib/controllers/home_page_controller.dart
@@ -0,0 +1,8 @@
+import 'package:pokedex_riverpod/models/page_data.dart';
+import 'package:riverpod/riverpod.dart';
+
+class HomePageController extends StateNotifier<HomePageData> {
+  HomePageController(
+    super._state,
+  );
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokedex_riverpod/controllers/home_page_controller.dart';
+import 'package:pokedex_riverpod/models/page_data.dart';
+import 'package:riverpod/riverpod.dart';
+
+final HomePageControllerProvider =
+    StateNotifierProvider<HomePageController, HomePageData>((ref) {
+  return HomePageController(
+    HomePageData.initial(),
+  );
+});
+
+class HomePage extends ConsumerStatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  late HomePageController _homePageController;
+  late HomePageData _homePageData;
+
+  @override
+  Widget build(BuildContext context) {
+    _homePageController = ref.watch(HomePageControllerProvider.notifier);
+    _homePageData = ref.watch(
+      HomePageControllerProvider,
+    );
+    return Scaffold(
+      body: _buildUI(
+        context,
+      ),
+    );
+  }
+
+  Widget _buildUI(
+    BuildContext context,
+  ) {
+    return SafeArea(
+        child: SingleChildScrollView(
+      child: Container(
+        width: MediaQuery.sizeOf(context).width,
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.sizeOf(context).width * 0.02),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _allPokmonsList(
+              context,
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+
+  Widget _allPokmonsList(
+    BuildContext context,
+  ) {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "All Pokemons",
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.60,
+            child: ListView.builder(
+                itemCount: 0,
+                itemBuilder: (context, index) {
+                  return ListTile();
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
